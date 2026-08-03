d337725307fbb317a0db8fcf3bb17d570f87f8a2
MimiA <asminerazafiarivelo@gmail.com>
2026-08-03 23:52:07 +0300

    feat(Reservation): implement reservation endpoint

--- src/main/java/com/hei/course/endpoint/rest/controller/ReservationController.java
+++ src/main/java/com/hei/course/endpoint/rest/controller/ReservationController.java
@@ -4,32 +4,31 @@ import com.hei.course.endpoint.rest.dto.ReservationInput;
 import com.hei.course.model.Reservation;
 import com.hei.course.security.UserPrincipal;
 import com.hei.course.service.ReservationService;
+import java.util.List;
+import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
-import java.util.UUID;
 
 @RequiredArgsConstructor
 @RequestMapping("/reservation")
 @RestController
 public class ReservationController {
-    private final ReservationService service;
-    @GetMapping()
-    public List<Reservation> getReservations() {
-        return service.getAllReservations();
-    }
+  private final ReservationService service;
 
-    @GetMapping("/{id}")
-    public Reservation getReservationById(
-            @PathVariable UUID id, @AuthenticationPrincipal UserPrincipal currentUser) {
-        return service.getReservationById(id, currentUser);
-    }
+  @GetMapping()
+  public List<Reservation> getReservations() {
+    return service.getAllReservations();
+  }
 
-    @PutMapping()
-    public Reservation saveReservation(@RequestBody ReservationInput input) {
-        return service.saveReservation(input);
-    }
+  @GetMapping("/{id}")
+  public Reservation getReservationById(
+      @PathVariable UUID id, @AuthenticationPrincipal UserPrincipal currentUser) {
+    return service.getReservationById(id, currentUser);
+  }
 
+  @PutMapping()
+  public Reservation saveReservation(@RequestBody ReservationInput input) {
+    return service.saveReservation(input);
+  }
 }
--- src/main/java/com/hei/course/service/UserService.java
+++ src/main/java/com/hei/course/service/UserService.java
@@ -4,44 +4,43 @@ import com.hei.course.entity.JUser;
 import com.hei.course.mapper.UserMapper;
 import com.hei.course.model.User;
 import com.hei.course.model.UserRole;
-import java.time.LocalDateTime;
-
 import com.hei.course.repository.UserRepository;
+import java.time.LocalDateTime;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
 
-    private final UserRepository userRepository;
-    private final PasswordEncoder passwordEncoder;
+  private final UserRepository userRepository;
+  private final PasswordEncoder passwordEncoder;
 
-    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
-        this.userRepository = userRepository;
-        this.passwordEncoder = passwordEncoder;
-    }
+  public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+    this.userRepository = userRepository;
+    this.passwordEncoder = passwordEncoder;
+  }
 
-    public User createUser(
-            String firstname,
-            String lastname,
-            LocalDateTime birthdate,
-            String email,
-            String phone,
-            String rawPassword,
-            UserRole userRole) {
+  public User createUser(
+      String firstname,
+      String lastname,
+      LocalDateTime birthdate,
+      String email,
+      String phone,
+      String rawPassword,
+      UserRole userRole) {
 
-        User user = new User();
-        user.setFirstname(firstname);
-        user.setLastname(lastname);
-        user.setBirthdate(birthdate);
-        user.setEmail(email);
-        user.setPhone(phone);
-        user.setPassword(passwordEncoder.encode(rawPassword));
-        user.setUserRole(userRole);
+    User user = new User();
+    user.setFirstname(firstname);
+    user.setLastname(lastname);
+    user.setBirthdate(birthdate);
+    user.setEmail(email);
+    user.setPhone(phone);
+    user.setPassword(passwordEncoder.encode(rawPassword));
+    user.setUserRole(userRole);
 
-        JUser entityToSave = UserMapper.toEntity(user);
-        JUser savedEntity = userRepository.save(entityToSave);
+    JUser entityToSave = UserMapper.toEntity(user);
+    JUser savedEntity = userRepository.save(entityToSave);
 
-        return UserMapper.toModel(savedEntity);
-    }
+    return UserMapper.toModel(savedEntity);
+  }
 }
