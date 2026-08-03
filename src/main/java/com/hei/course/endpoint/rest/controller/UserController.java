cb282b3f3e651470e1503b23a6345398902158ec
Miriasme <asminerazafiarivelo@gmail.com>
2026-08-03 23:22:48 +0300

    feat(USerController): add endpoint to register user

---
+++ src/main/java/com/hei/course/endpoint/rest/controller/UserController.java
@@ -0,0 +1,35 @@
+package com.hei.course.endpoint.rest.controller;
+
+import com.hei.course.endpoint.rest.dto.UserInput;
+import com.hei.course.model.User;
+import com.hei.course.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/users")
+    public ResponseEntity<User> createUser(@RequestBody UserInput request) {
+        User user = userService.createUser(
+                request.firstname(),
+                request.lastname(),
+                request.birthdate(),
+                request.email(),
+                request.phone(),
+                request.password(),
+                request.userRole()
+        );
+        return ResponseEntity.status(HttpStatus.CREATED).body(user);
+    }
+}
